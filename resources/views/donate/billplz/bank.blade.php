522c8c3688e3ea3085369018c251980ce2df6e63
nblkmal <nblkmal@gmail.com>
2021-12-14 17:49:57 +0800

    add billplz form & bank

---
+++ resources/views/donate/billplz/bank.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+
+@section('content')
+<div class="w-4/5 my-16">
+    <div class="bg-white rounded-xl">
+        <div class="max-w-7xl mx-auto py-12 sm:px-6 lg:py-16 lg:px-1 lg:flex lg:items-center lg:justify-between">
+            <h2 class="flex inline-flex items-center text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
+                <span>Donate to us!</span>
+            </h2>
+        </div>
+    </div>
+    <div class="bg-white justify-center mx-auto rounded-xl my-5 w-1/2 py-5 px-5">
+        {{ $donation }}
+        <div class="overflow-auto lg:overflow-visible sm:overflow-hidden ">
+			<form method="POST" action="{{ route('donate:create') }}">
+                @csrf
+                <div class="grid grid-cols-4 gap-4 my-4">
+                    @foreach ($banks as $bank)
+                        <a href="{{ route('donate:billplz:create:bill', [$donation, $bank['code']]) }}" class="border rounded-lg px-auto py-3 text-center hover:bg-slate-400">{{ $bank['category'] }}{{ $bank['code'] }}</a>
+                    @endforeach
+                </div>
+                
+            </form>
+		</div>
+    </div>
+    
+</div>
+@endsection
--- resources/views/donate/index.blade.php
+++ resources/views/donate/index.blade.php
@@ -100,6 +100,102 @@
             </form>
 		</div>
     </div>
+    <div class="bg-white justify-center mx-auto rounded-xl my-5 w-1/2 py-5 px-5">
+        Billplz
+        <div class="overflow-auto lg:overflow-visible sm:overflow-hidden ">
+			<form method="POST" action="{{ route('donate:billplz:create') }}">
+                @csrf
+                <div class="grid grid-cols-2 gap-4 my-4">
+                    <div class="my-2">
+                        <label for="company-website" class="block text-sm font-medium text-gray-700">
+                            Name
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                </svg>
+                            </span>
+                            <input type="text" name="name" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <label for="company-website" class="block text-sm font-medium text-gray-700">
+                            Email
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                                </svg>
+                            </span>
+                            <input type="email" name="email" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <label for="company-website" class="block text-sm font-medium text-gray-700">
+                            Phone
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                </svg>+60
+                            </span>
+                            <input type="text" name="phone" class="@error('phone') is-invalid @enderror focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="123456789">
+                        </div>
+                        @error('phone')
+                            <small class="text-red-700">{{ $message }}</small>
+                        @enderror
+
+                    </div>
+                    <div class="my-2">
+                        <label for="company-website" class="block text-sm font-medium text-gray-700">
+                            Note <small>(*optional)</small>
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
+                            </span>
+                            <input type="text" name="note" id="note" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                        </div>
+                    </div>
+                </div>
+                <div class="grid grid-cols-3 gap-4">
+                    <div></div>
+                    <div>
+                        <label for="company-website" class="block text-sm font-medium text-gray-700">
+                            Amount
+                        </label>
+                        <div class="mt-1 flex rounded-md shadow-sm">
+                            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
+                            </span>
+                            <input type="text" name="amount" class="@error('amount') is-invalid @enderror border-transparent focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="www.example.com">
+                        </div>
+                        @error('amount')
+                            <small class="text-red-700">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div></div>
+                </div>
+
+                <div class="flex w-full">
+                    <button type="submit" class="flex mt-2 items-center justify-center focus:outline-none mt-4 mb-3 w-full bg-green-700 hover:bg-green-500 text-white py-2 rounded-md transition duration-100">
+                        <span class="mr-2">Donate now</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                </div>
+                
+            </form>
+		</div>
+    </div>
     
 </div>
 @endsection
